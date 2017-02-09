a7f27dc3de7ff520110b53b8cc734d0a73991212
Samyak Puri <samyakpuri@hotmail.com>
2017-02-09 11:01:13 +0530

    MVC Filters

--- DAY10/MvcDay3/MvcDay3/Controllers/HomeController.cs
+++ DAY10/MvcDay3/MvcDay3/Controllers/HomeController.cs
@@ -7,12 +7,15 @@ using System.Web.Mvc;
 
 namespace MvcDay3.Controllers
 {
+    [CustomExceptionFilter]
     public class HomeController : Controller
     {
         // GET: Home
         [TimeElapsedFilter]
-        public ActionResult Index()
+        public ActionResult Index(int a,int b)
         {
+           // int a = 1, b = 0;
+            int c = a / b;
             return View();
         }
     }
