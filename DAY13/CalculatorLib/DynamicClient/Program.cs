a26ba23da99ac82b97bea878bf17fccf311832bf
Samyak Puri <samyakpuri@hotmail.com>
2017-02-21 15:42:25 +0530

    API connection without wsdl

---
+++ DAY13/CalculatorLib/DynamicClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChannelFactory<CalculatorLib.ICalculator> channelFactory = new ChannelFactory<CalculatorLib.ICalculator>(new BasicHttpBinding(), "http://localhost:33333/basic");
+            CalculatorLib.ICalculator client = channelFactory.CreateChannel();
+            var response = client.Operation(new CalculatorLib.Request() {Operation = CalculatorLib.Operator.Sum, Value1 = 2, Value2 = 3 });
+
+            Console.WriteLine(response.Result);
+            Console.ReadLine();
+        }
+    }
+}
