e19d0eff6bd48e34c8511bee97b12705877a901a
Samyak Puri <samyakpuri@hotmail.com>
2017-02-21 14:19:15 +0530

    Multiple Service and Multiple Endpoints WCF

---
+++ DAY13/CalculatorLib/CalculatorLib/FullCalculatorService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLib
+{
+    class FullCalculatorService : IFullCalculator
+    {
+        public Response Operate(Request request)
+        {
+            Response response = new Response { Status = Status.Success };
+            switch (request.Operation)
+            {
+                case Operator.None:
+                    response.Status = Status.Failiure;
+                    break;
+                case Operator.Sum:
+                    response.Result = (double)(request.Value1 + request.Value2);
+                    break;
+                case Operator.Subtract:
+                    response.Result = (double)(request.Value1 - request.Value2);
+                    break;
+                case Operator.Multiply:
+                    response.Result = (double)(request.Value1 * request.Value2);
+                    break;
+                case Operator.Divide:
+                    if (request.Value2 != 0)
+                        response.Result = (double)(request.Value1 / request.Value2);
+                    else
+                        response.Status = Status.Failiure;
+                    break;
+                case Operator.Power:
+                    response.Result = (double)Math.Pow((double)request.Value1, (double)request.Value2);
+                    break;
+                case Operator.Root:
+                    response.Result = (double)Math.Pow((double)request.Value1, (1.0 / (double)request.Value2));
+                    break;
+                default:
+                    response.Status = Status.Failiure;
+                    break;
+            }
+            return response;
+        }
+    }
+}
--- DAY13/CalculatorLib/WebServiceClient/Program.cs
+++ DAY13/CalculatorLib/WebServiceClient/Program.cs
@@ -10,9 +10,13 @@ namespace WebServiceClient
     {
         static void Main(string[] args)
         {
-            Calculator.CalculatorClient client = new Calculator.CalculatorClient();
+            Calculator.CalculatorClient client = new Calculator.CalculatorClient("BasicHttp");
             var response = client.Operation(new Calculator.Request() {Operation = Calculator.Operator.Sum, Value1 = 10, Value2 = 20 });
-            Calculator.AdvanceCalculatorClient client2 = new Calculator.AdvanceCalculatorClient();
+            Calculator.AdvanceCalculatorClient client2 = new Calculator.AdvanceCalculatorClient("BasicHttp_IAdvance");
+            var response2 = client2.AdvanceOperations(new Calculator.Request() {Operation = Calculator.Operator.Power, Value1 = 3, Value2 = 3});
+
+            FullCalculator.FullCalculatorClient client3 = new FullCalculator.FullCalculatorClient("BasicHttp_Full");
+            var response3 = client3.Operate(new FullCalculator.Request() { Operation = FullCalculator.Operator.Power, Value1 = 10, Value2 = 2 });
         }
     }
 }
