0b8092ce4fd0ab8b3178bf5d1c326bb042bafc2b
Samyak Puri <samyakpuri@hotmail.com>
2017-04-06 12:41:25 +0530

    Commands WPF

--- Day16/HeadStartMvvm/HeadStartMvvm/ViewModels/PersonViewModel.cs
+++ Day16/HeadStartMvvm/HeadStartMvvm/ViewModels/PersonViewModel.cs
@@ -4,16 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HeadStartMvvm.Models;
+using HeadStartMvvm.Commands;
 
 namespace HeadStartMvvm.ViewModels
 {
     class PersonViewModel
     {
         public Person Person { get; set; }
+        public AddPersonCommand AddPersonCommand { get; set; }
 
         public PersonViewModel()
         {
             Person = new Person();
+            AddPersonCommand = new AddPersonCommand();
         }
     }
 }
