f47260cd11b6c0c4f61bf2c22ae00be89f6ceb0e
Samyak Puri <samyakpuri@hotmail.com>
2017-04-06 10:37:45 +0530

    Triggers in Style tag

---
+++ Day15/WpfDay2/WpfDay2/Converters/NameConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfDay2.Converters
+{
+    class NameConverter: IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (values.Any(v => v == null || v == DependencyProperty.UnsetValue || String.IsNullOrEmpty(v.ToString())))
+                return null;
+
+            if(parameter != null)
+            {
+                if (parameter.ToString().Equals("US"))
+                {
+                    return values.Select(v => v.ToString()).Aggregate((a, b) => b + "," + a);
+                }
+            }
+
+            return values.Select(v => v.ToString()).Aggregate((a, b) => a + " " + b);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+                return null;
+
+            List<String> stringResult = new List<String>();
+            String stringValue = value.ToString();
+            stringResult = stringValue.Split(null).ToList();
+            if (stringResult.Count == 1)
+                stringResult.Add(" ");
+            object[] objectResult = stringResult.Cast<object>().ToArray();
+            return objectResult;
+        }
+    }
+}
---
+++ Day16/WpfDay3/WpfDay3/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfDay3
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+
+        }
+
+        public void ChangeTheme(string theme)
+        {
+            if(theme == "US")
+            {
+                App.Current.Resources.MergedDictionaries.Add(new ResourceDictionary() { Source = new Uri("Theme2.xaml", UriKind.Relative) });
+            }
+            else
+            {
+                App.Current.Resources.MergedDictionaries.Add(new ResourceDictionary() { Source = new Uri("Theme1.xaml", UriKind.Relative) });
+            }
+        }
+    }
+}
