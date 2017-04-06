cb7590b50541c8ec41d98811220c8e4647880437
Samyak Puri <samyakpuri@hotmail.com>
2017-04-06 14:43:55 +0530

    WPF Validations

---
+++ Day16/HeadStartMvvm/HeadStartMvvm/Validators/MandatoryFieldRule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace HeadStartMvvm.Validators
+{
+    class MandatoryFieldRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            if (value.ToString() == "")
+                return new ValidationResult(false, "Field is Mandatory");
+            return new ValidationResult(true,null);
+        }
+    }
+}
