215bf1e826dd9c57a871948ab6dc17dbb78de660
Samyak Puri <samyakpuri@hotmail.com>
2017-04-07 12:09:41 +0530

    Delgate Commands

---
+++ Day17/MvvmDay2/MvvmDay2/Commands/DelegateCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MvvmDay2.Commands
+{
+    class DelegateCommand : ICommand
+    {
+        Predicate<object> _predicate;
+        Action<object> _action;
+
+        public DelegateCommand(Predicate<object> predicate,Action<object> action)
+        {
+            _predicate = predicate;
+            _action = action;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return _predicate(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            _action(parameter);
+        }
+    }
+}
--- Day17/MvvmDay2/MvvmDay2/ViewModels/MainViewModel.cs
+++ Day17/MvvmDay2/MvvmDay2/ViewModels/MainViewModel.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MvvmDay2.Models;
+using MvvmDay2.Commands;
+using System.Windows;
 
 namespace MvvmDay2.ViewModels
 {
@@ -11,11 +13,23 @@ namespace MvvmDay2.ViewModels
     {
         public Header Header { get; set; }
         public Person Person { get; set; }
+        public DelegateCommand AddPersonCommand { get; set; }
 
         public MainViewModel()
         {
             Header = new Header() { Title = "Hello World", Url = new Uri("../Images/logo.png",UriKind.Relative) };
             Person = new Person() { Name = "Samyak Puri", Gender = true, Id = 1};
+            AddPersonCommand = new DelegateCommand(CanExecute,Execute);
+        }
+
+        private void Execute(object obj)
+        {
+            MessageBox.Show("Saved");
+        }
+
+        private bool CanExecute(object obj)
+        {
+            return true;
         }
     }
 }
