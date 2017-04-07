57fc28720954dafb6fde2c36457dab586ccb946c
Samyak Puri <samyakpuri@hotmail.com>
2017-04-07 14:40:32 +0530

    Mock for testing Messagebox

---
+++ Day17/MvvmDay2/MvvmDay2/Services/DialogService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MvvmDay2.Services
+{
+    class DialogService : IDialogService
+    {
+        public void Show(string message)
+        {
+            MessageBox.Show(message);
+        }
+    }
+
+    public interface IDialogService
+    {
+        void Show(string message);
+    }
+}
