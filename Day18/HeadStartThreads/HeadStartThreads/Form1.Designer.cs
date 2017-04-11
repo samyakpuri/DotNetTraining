be60203007d16ca627d43677c63421b5cb776595
Samyak Puri <samyakpuri@hotmail.com>
2017-04-11 12:05:16 +0530

    Started locks

--- Day18/HeadStartThreads/HeadStartThreads/Form1.Designer.cs
+++ Day18/HeadStartThreads/HeadStartThreads/Form1.Designer.cs
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnLongSynchronusTask = new System.Windows.Forms.Button();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnLongSynchronusTask
+            // 
+            this.btnLongSynchronusTask.Location = new System.Drawing.Point(56, 94);
+            this.btnLongSynchronusTask.Name = "btnLongSynchronusTask";
+            this.btnLongSynchronusTask.Size = new System.Drawing.Size(159, 23);
+            this.btnLongSynchronusTask.TabIndex = 0;
+            this.btnLongSynchronusTask.Text = "Long Synchronus Task";
+            this.btnLongSynchronusTask.UseVisualStyleBackColor = true;
+            this.btnLongSynchronusTask.Click += new System.EventHandler(this.btnLongSynchronusTask_Click);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(67, 52);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(35, 13);
+            this.lblTime.TabIndex = 1;
+            this.lblTime.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.btnLongSynchronusTask);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnLongSynchronusTask;
+        private System.Windows.Forms.Label lblTime;
     }
 }
 
--- Day18/HeadStartThreads/HeadStartThreads/Form1.cs
+++ Day18/HeadStartThreads/HeadStartThreads/Form1.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,48 @@ namespace HeadStartThreads
 {
     public partial class Form1 : Form
     {
+        DateTime start;
+        int _counter = 0;
+        object _locker = new object();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnLongSynchronusTask_Click(object sender, EventArgs e)
+        {
+            //Print();
+            start = DateTime.Now;
+            for (int i = 0; i < 500; i++)
+            {
+                Thread thread = new Thread(new ParameterizedThreadStart(Print));
+                thread.Start(i);
+            }
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    ThreadPool.QueueUserWorkItem(new WaitCallback(Print),i);
+            //}
+            
+        }
+
+        private void Print(object number)
+        {
+            //Stopwatch watch = new Stopwatch();
+            //watch.Start();
+            int num = Convert.ToInt32(number);
+            int end = (num + 1) * 500;
+            for (int i = num*500; i < end; i++)
+            {
+                Console.WriteLine(i);
+            }
+            lock (_locker)
+	        {
+		        _counter++;
+	        }
+            //watch.Stop();
+            //MessageBox.Show(watch.ElapsedMilliseconds.ToString());
+            this.Invoke(new MethodInvoker(() => lblTime.Text = (DateTime.Now - start).ToString()));
         }
     }
 }
