07291c108998d57ebdf62c94a8d95da33b26efee
Samyak Puri <samyakpuri@hotmail.com>
2017-04-13 13:15:58 +0530

    Changed server to local mdf file

--- EmployeeManagerPrism/EmployeeManagerPrism/Mappers/EmployeeMapper.cs
+++ EmployeeManagerPrism/EmployeeManagerPrism/Mappers/EmployeeMapper.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EmployeeManagerPrism.Core.Models;
+using EmployeeManagerPrism.DataModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
